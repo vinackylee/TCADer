4326255761b3d4bf9eb5cd8ace8c035592381613
vinacky <vinacky@163.com>
2022-11-23 21:50:20 +0800

    Update run.sh

--- sim_test/run.sh
+++ sim_test/run.sh
@@ -1,1 +1,1 @@
-~/work/chipyard/riscv-tools-install/riscv64/bin/riscv64-unknown-elf-gcc -DPREALLOCATE=1 -mcmodel=medany -std=gnu99 -O2 -ffast-math -fno-common -fno-builtin-printf -Bstatic -nostdlib -nostartfiles -lm -lgcc -T test.ld testaes.c common/syscalls.c common/crt.S -I . 
+riscv64-unknown-elf-gcc -DPREALLOCATE=1 -mcmodel=medany -std=gnu99 -O2 -ffast-math -fno-common -fno-builtin-printf -Bstatic -nostdlib -nostartfiles -lm -lgcc -T test.ld testaes.c common/syscalls.c common/crt.S -I . 
